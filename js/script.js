8416cc53c27c4c4161216d22c95678f5a83da87b
Nancy Tran <nhvt@uw.edu>
2017-05-21 00:35:45 -0700

    Fix directions

--- js/script.js
+++ js/script.js
@@ -38,32 +38,32 @@ $(document).ready(function() {
     if ((x1 !== event.pageX) && (y1 !== event.pageY)) {
       // check the direction (with a margin error of 20 degrees)
       if (angle > 20 && angle <= 65) {
-        console.log("direction: SE");
-      } else if (angle > 65 && angle <= 115) {
-        console.log("direction: S");
-      } else if (angle > 115 && angle <= 160) {
-        console.log("direction: SW");
-      } else if (angle > 160 && angle <= 200) {
-        console.log("direction: W");
-      } else if (angle > 200 && angle <= 250) {
         console.log("direction: NW");
-      } else if (angle > 250 && angle <= 290) {
+      } else if (angle > 65 && angle <= 115) {
         console.log("direction: N");
-      } else if (angle > 290 && angle <= 340) {
+      } else if (angle > 115 && angle <= 160) {
         console.log("direction: NE");
-      } else { // angle > 340 or angle is <= 20
+      } else if (angle > 160 && angle <= 200) {
         console.log("direction: E");
+      } else if (angle > 200 && angle <= 250) {
+        console.log("direction: SE");
+      } else if (angle > 250 && angle <= 290) {
+        console.log("direction: S");
+      } else if (angle > 290 && angle <= 340) {
+        console.log("direction: SW");
+      } else { // angle > 340 or angle is <= 20
+        console.log("direction: W");
       }
     } else if ((x1 === event.pageX && y1 !== event.pageY) || (x1 !== event.pageX && y1 === event.pageY)) {
       // if the X-axis is the same, but Y-axis isn't (or vice versa), check if the angle is exactly N, E, S, or W
       if (angle === 0) {
-        console.log("direction: E");
-      } else if (angle === 90) {
-        console.log("direction: S");
-      } else if (angle === 180) {
         console.log("direction: W");
-      } else { // angle === 270
+      } else if (angle === 90) {
         console.log("direction: N");
+      } else if (angle === 180) {
+        console.log("direction: E");
+      } else { // angle === 270
+        console.log("direction: S");
       }
     }
   });
